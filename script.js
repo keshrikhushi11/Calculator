59b486c216570ec18795ee15dc7a32d2899937b9
Khushi Kumari <keshri.khushi06@gmail.com>
2026-04-17 23:25:48 +0530

    My calculator project

---
+++ script.js
@@ -0,0 +1,28 @@
+function append(value) {
+  document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
+
+function calculate() {
+  try {
+    let result = eval(document.getElementById("display").value);
+    document.getElementById("display").value = result;
+  } catch {
+    alert("Invalid Expression");
+  }
+}
+
+// Keyboard support
+document.addEventListener("keydown", function(e) {
+  if (!isNaN(e.key) || "+-*/().".includes(e.key)) {
+    append(e.key);
+  }
+  if (e.key === "Enter") calculate();
+  if (e.key === "Backspace") {
+    let val = display.value;
+    display.value = val.slice(0, -1);
+  }
+});

8058c6e1e56ed9b6b8254cb84c3c6ccf9ac1af7c
Khushi Kumari <keshri.khushi06@gmail.com>
2026-05-02 15:24:21 +0530

    Updated my calculator with scientific features

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let history = [];
 const display = document.getElementById("display");
 function append(value) {
   display.value += value;
@@ -10,13 +11,39 @@ function clearDisplay() {
 function calculate() {
   try {
     let expression = display.value;
+
     if (expression.trim() === "") return;
+
+    expression = expression
+      .replace(/Math\.sin\(([^)]+)\)/g, 'Math.sin(($1) * Math.PI / 180)')
+      .replace(/Math\.cos\(([^)]+)\)/g, 'Math.cos(($1) * Math.PI / 180)')
+      .replace(/Math\.tan\(([^)]+)\)/g, 'Math.tan(($1) * Math.PI / 180)');
+
     let result = Function('"use strict"; return (' + expression + ')')();
+
+    //SAVE HISTORY
+    let record = display.value + " = " + result;
+    history.push(record);
+
+    updateHistory();
+
     display.value = result;
-  } catch(error) {
+
+  } catch (error) {
     display.value = "Error";
   }
 }
 function deleteLast(){
   display.value = display.value.slice(0, -1);
+}
+
+function updateHistory() {
+  let list = document.getElementById("historyList");
+  list.innerHTML = "";
+
+  history.slice().reverse().forEach(item => {
+    let li = document.createElement("li");
+    li.textContent = item;
+    list.appendChild(li);
+  });
 }
